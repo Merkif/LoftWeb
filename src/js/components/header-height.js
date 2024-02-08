073e12e872bfc6c94748aaf6f4836729c36cd4fa
Merkif <nokiaman1997@gmail.com>
2024-02-08 07:31:54 +0300

    Оптимизация кода

--- src/js/components/header-height.js
+++ src/js/components/header-height.js
@@ -1,6 +1,21 @@
-const getHeaderHeight = () => {
+function setHeaderHeight() {
   const headerHeight = document?.querySelector('.header')?.offsetHeight;
   document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
 }
-getHeaderHeight();
-window.addEventListener('resize', getHeaderHeight);
+
+document.addEventListener('DOMContentLoaded', setHeaderHeight);
+
+function debounce(func, delay) {
+  let timeoutId;
+  return function() {
+      const context = this;
+      const args = arguments;
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(function() {
+          func.apply(context, args);
+      }, delay);
+  };
+}
+
+const debouncedSetHeaderHeight = debounce(setHeaderHeight, 250);
+window.addEventListener('resize', debouncedSetHeaderHeight);
