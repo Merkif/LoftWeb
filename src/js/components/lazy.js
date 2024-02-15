e492cf46b7536ec2dbe7cfdb7e4a793fa133273d
sergeykraynov <kraynov.sergey@gmail.com>
2024-02-15 17:56:13 +0300

    Добавил обновление Lazy после AJAX-запроса в фильтре

--- src/js/components/lazy.js
+++ src/js/components/lazy.js
@@ -1,2 +1,12 @@
 import LazyLoad from "vanilla-lazyload";
 let lazyLoadInstance = new LazyLoad({});
+
+/* Проверяем наличие jQuery */
+window.addEventListener('load', () => {
+    if (window.jQuery) {
+        /* Обновляем Lazy по AJAX-событию в фильтре */
+        $(document).on('mse2_load', function (e, data) { 
+            lazyLoadInstance.update();
+        });
+    }
+})
